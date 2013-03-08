08c1ed486cc8e876432ed0f3d2d9208fab576a8c
davidsteinsland <david@davidsteinsland.net>
2013-03-08 15:02:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,55 @@ log-viewer-js
 =============
 
 A package used to read log file, e.g. with tail
+
+## Simple Usage
+```javascript
+var plain = [
+	"Hello, World",
+	"What are you doing?"
+];
+
+var parsed = new Log.Parser (plain).parse();
+```
+
+## PHP / Apache logs
+```javascript
+var httpd = [
+	'[Fri Mar 08 10:33:51 2013] [info] [client 127.0.0.1] Hello, World',
+	'[Fri Mar 08 10:33:51 2013] [info] [client 127.0.0.1] Hello, World',
+	'[Fri Mar 08 10:33:51 2013] [info] [client 127.0.0.1] Hello, World',
+	'[Fri Mar 08 10:33:51 2013] [info] [client 81.166.191.110] Multiple lines',
+	', or it that even possible?',
+],
+php = [
+	'[07-Mar-2013 13:02:23 Europe/Oslo] Fatal Error: Expected ")" got ","',
+	'Stack Trace:',
+	'#0 main()',
+	'#1 index.php',
+	'#2 somefile.php',
+	'[07-Mar-2013 13:02:23 Europe/Oslo] Notice: $data not defined in this scope',
+];
+
+var formatter = new Log.Formatter ({
+	onLine: function (line)
+	{
+		// a basic HTML formatter
+		return '<li>' + line + '</li>';
+	}
+});
+
+formatter.ifMatch (function (line)
+{
+	if ( ! /Multiple/.test(line))
+		return false;
+	return '<em>' + line + '</em>';
+});
+
+var apache = Log.Apache(httpd, formatter),
+php = Log.Php(php, new Log.Formatter({
+	onLine: function (line)
+	{
+		return '<li>' + line.replace (/[\n]+/g, "<br />") + '</li>';
+	}
+}));
+```
---
+++ log.js
@@ -0,0 +1,181 @@
+/**
+ * @package Log
+ * @contains Log.Parser, Log.Formatter
+ * @author David Steinsland
+ * 
+ * Parses a log file and generates a HTML list for display
+ * The lines are parsed by every date, so that it will capture most formats such as:
+ * Apache error/access log, php log, mysql, etc.
+ * A custom format may also be applied.
+ * 
+ * Apache: [Fri Mar 08 10:33:51 2013]
+ * Php: [07-Mar-2013 13:02:23 Europe/Oslo]
+ * 
+ * A LogFormatter object can also be attached to every line, making special messages "pop" out, for instance
+ * lines containing the word "error", etc.
+ */
+
+function extend(a, b)
+{
+	for(var key in b)
+		if (b.hasOwnProperty (key))
+			a[key] = b[key];
+	return a;
+}
+
+var Log = {};
+
+/**
+ * Helper classes
+ */
+Log.Apache = function (data, formatter)
+{
+	var log = new Log.Parser (data, {
+		newLine: function (line)
+		{
+			return /^\[(?:\w{3} ){2}\d{2} (?:\d{2}:){2}\d{2} \d{4}\]/.test(line);
+		}
+	});
+	
+	return log.parse (formatter);
+};
+Log.Php = function (data, formatter)
+{
+	var log = new Log.Parser (data, {
+		newLine: function (line)
+		{
+			return /^\[\d{2}-[a-zA-Z]{3}-\d{4} \d{2}:\d{2}:\d{2}(?: [A-Za-z\/]+)*\]/.test(line);
+		}
+	});
+	
+	return log.parse (formatter);
+};
+
+Log.Iterator = function (data, keysOnly = false)
+{
+	var pos = 0, length = data.length;
+	
+	return {
+		current: function ()
+		{
+			return data[pos];
+		},
+		hasNext: function ()
+		{
+			return length > pos;
+		},
+		peek: function (pos)
+		{
+			if ( pos < 0 || pos >= length)
+				throw "Invalid position";
+			return data[pos];
+		},
+		next: function ()
+		{
+			if ( ! this.hasNext() )
+				throw "Stop iterating";
+			if (keysOnly)
+				return pos++;
+			return [pos, data[pos++]];
+		},
+		rewind: function ()
+		{
+			pos = 0;
+		}
+	};
+};
+
+/**
+ * @param data	an array containing each line of the file
+ */
+Log.Parser = function (data, options)
+{
+	var $this = this;
+	this.iterator = new Log.Iterator (data);
+	this.options = extend ({
+		/**
+		 * Whether or not the current line is a new message in the log, or if it is just a continuing of
+		 * the previous message.
+		 * 
+		 * @return bool if the line is a new message or not. Default: false
+		 */
+		newLine: function (line)
+		{
+			return false;
+		},
+	}, options || {});
+	
+	return {
+		parse: function (formatter)
+		{
+			var lines = [];
+			try
+			{
+				var line, tmpLine;
+				while ( (line = $this.iterator.next()) )
+				{
+					// group all error messages that might run for more than 1 line
+					while ( $this.iterator.hasNext() && ! $this.options.newLine ($this.iterator.peek(line[0] + 1)) )
+					{
+						line[1] += "\n" + $this.iterator.next()[1];
+					}
+					
+					lines.push ( line[1] );
+				}
+			}
+			// catch (err if err instanceof StopIteration)
+			catch (err)
+			{
+				// no more lines
+			}
+			
+			formatter = formatter || new Log.Formatter();
+			return formatter.format (lines);
+		}
+	};
+};
+
+Log.Formatter = function (options)
+{
+	options = extend ({
+		onLine: function (line)
+		{
+			return line + "\n";
+		},
+	}, options || {});
+	
+	var conditionalFormats = [];
+	
+	return {
+		format: function (lines)
+		{
+			var result = "",
+				line,
+				lineIterator = new Log.Iterator(lines),
+				formatIterator = new Log.Iterator(conditionalFormats);
+			
+			while (lineIterator.hasNext())
+			{
+				line = lineIterator.next()[1];
+				
+				formatIterator.rewind();
+				var cond;
+				while (formatIterator.hasNext())
+				{
+					cond = formatIterator.next()[1];
+					if ( cond (line) )
+						line = cond(line);
+				}
+				
+				result += options.onLine (line);
+			}
+			
+			return result;
+		},
+		
+		ifMatch: function (callback)
+		{
+			conditionalFormats.push (callback);
+		}
+	};
+};
